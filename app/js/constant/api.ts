764ab24382ffca51f63ba6b9e17e91f99fd4aeb4
Derek Meulmeester <derek.meulmeester@shopify.com>
2017-03-25 11:24:22 -0400

    Use HTTPS api

--- app/js/constant/api.ts
+++ app/js/constant/api.ts
@@ -3,7 +3,7 @@ module calypso.Const.API {
     export const DEFINITION_ACCEPT_HEADER = 'application/vnd.iuclid6.ext+json;type=iuclid6.Definition';
     export const DOCUMENT_CONTENT_TYPE_HEADER = 'application/vnd.iuclid6.ext+json; type=iuclid6.Document';
 
-    export const BASE_URL = 'http://iuclid.ca:3000';
+    export const BASE_URL = 'https://iuclid.ca/api';
     export const BASE_URI = `${BASE_URL}/iuclid6-ext/api/ext/v1`;
     export const BASE_DEFINITIONS_URI = `${BASE_URI}/definition`;
     export const BASE_API_URI = `${BASE_URI}/query/iuclid6`;
